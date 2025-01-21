da528e1bb518cb5f8a9ab694020c1fcf0ed21353
Sofia Pereira <73718358+SofiaPereira18@users.noreply.github.com>
2025-01-21 15:42:51 +0000

    installed Router, added HomeView and EducationView

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Portfolio
+# portfolio
+
+
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<script setup>
+  import { RouterLink, useRoute } from 'vue-router';
+  
+</script>
+
+<template>
+    <!-- About -->
+    <RouterLink
+    to="/"
+    >
+    <span>About</span>    
+    </RouterLink>
+
+    <!-- Education -->
+   <RouterLink
+    to="/"
+    >
+    <span>Education</span>    
+    </RouterLink>
+
+   <!-- Work -->
+   <RouterLink
+    to="/"
+    >
+    <span>Work</span>    
+    </RouterLink>
+
+</template>
